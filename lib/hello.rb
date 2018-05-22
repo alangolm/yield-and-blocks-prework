8350f813a3da7826edb4df0a579723076abf885e
Alan Golman <golman.alan@gmail.com>
2018-05-22 19:34:56 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,8 +6,11 @@ def hello_t(array)
       yield array[i]
       i += 1 
     end 
-  array 
-  
+    
+    array
+  else 
+    puts "Hey! No block was given!"
+  end   
 end
 
 # call your method here!
